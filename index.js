88d27b37e5290f0d9d187623b3e936006a57fc37
Feniks-DDoS <valentindikhtyarenko2004@gmail.com>
2025-02-11 01:50:13 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 1000,
+    effect: "cards",
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  }
+)
